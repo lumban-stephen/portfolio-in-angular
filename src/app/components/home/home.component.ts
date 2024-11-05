a90af95830a2f3a2330ab1cb05a568261bf68fa5
Stephen Lumban <stephenlumban@gmail.com>
2024-11-05 10:59:38 +0800

    change icons and click functions

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,4 +15,14 @@ export class HomeComponent {
   goToLink(url: string): void {
     window.open(url, '_blank');
   }
+
+  smoothScrollTo(sectionId: string, event: Event) {
+    event.preventDefault(); // Prevent the default jump behavior
+
+    // Locate the section by ID and scroll to it smoothly
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
